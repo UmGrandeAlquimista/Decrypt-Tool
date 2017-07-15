d3df7b1afd4f1ba3b6cd115116b708c45f2c692d
Hiro <hirohamada@post.com>
2017-07-15 00:39:50 -0300

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,210 @@
+﻿namespace DES
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.button1 = new System.Windows.Forms.Button();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.richTextBox2 = new System.Windows.Forms.RichTextBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.richTextBox3 = new System.Windows.Forms.RichTextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.ID = new System.Windows.Forms.Label();
+            this.tamanho = new System.Windows.Forms.Label();
+            this.comprimido = new System.Windows.Forms.Label();
+            this.Setid = new System.Windows.Forms.Label();
+            this.SetLen = new System.Windows.Forms.Label();
+            this.SetCompress = new System.Windows.Forms.Label();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(657, 345);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(78, 27);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "descrypt";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(174, -1);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(561, 193);
+            this.richTextBox1.TabIndex = 1;
+            this.richTextBox1.Text = "";
+            // 
+            // richTextBox2
+            // 
+            this.richTextBox2.Location = new System.Drawing.Point(1, 191);
+            this.richTextBox2.Name = "richTextBox2";
+            this.richTextBox2.Size = new System.Drawing.Size(405, 180);
+            this.richTextBox2.TabIndex = 2;
+            this.richTextBox2.Text = "";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(411, 349);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(240, 20);
+            this.textBox1.TabIndex = 3;
+            // 
+            // richTextBox3
+            // 
+            this.richTextBox3.Location = new System.Drawing.Point(405, 191);
+            this.richTextBox3.Name = "richTextBox3";
+            this.richTextBox3.Size = new System.Drawing.Size(330, 152);
+            this.richTextBox3.TabIndex = 4;
+            this.richTextBox3.Text = "";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(5, 163);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(104, 15);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Created@ By Hiro";
+            // 
+            // ID
+            // 
+            this.ID.AutoSize = true;
+            this.ID.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ID.Location = new System.Drawing.Point(12, 12);
+            this.ID.Name = "ID";
+            this.ID.Size = new System.Drawing.Size(24, 16);
+            this.ID.TabIndex = 6;
+            this.ID.Text = "ID:";
+            // 
+            // tamanho
+            // 
+            this.tamanho.AutoSize = true;
+            this.tamanho.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tamanho.Location = new System.Drawing.Point(9, 38);
+            this.tamanho.Name = "tamanho";
+            this.tamanho.Size = new System.Drawing.Size(69, 16);
+            this.tamanho.TabIndex = 7;
+            this.tamanho.Text = "Tamanho:";
+            // 
+            // comprimido
+            // 
+            this.comprimido.AutoSize = true;
+            this.comprimido.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.comprimido.Location = new System.Drawing.Point(11, 64);
+            this.comprimido.Name = "comprimido";
+            this.comprimido.Size = new System.Drawing.Size(84, 16);
+            this.comprimido.TabIndex = 8;
+            this.comprimido.Text = "Comprimido:";
+            // 
+            // Setid
+            // 
+            this.Setid.AutoSize = true;
+            this.Setid.Location = new System.Drawing.Point(39, 15);
+            this.Setid.Name = "Setid";
+            this.Setid.Size = new System.Drawing.Size(0, 13);
+            this.Setid.TabIndex = 9;
+            // 
+            // SetLen
+            // 
+            this.SetLen.AutoSize = true;
+            this.SetLen.Location = new System.Drawing.Point(78, 41);
+            this.SetLen.Name = "SetLen";
+            this.SetLen.Size = new System.Drawing.Size(0, 13);
+            this.SetLen.TabIndex = 10;
+            // 
+            // SetCompress
+            // 
+            this.SetCompress.AutoSize = true;
+            this.SetCompress.Location = new System.Drawing.Point(94, 67);
+            this.SetCompress.Name = "SetCompress";
+            this.SetCompress.Size = new System.Drawing.Size(0, 13);
+            this.SetCompress.TabIndex = 11;
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(12, 134);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(51, 17);
+            this.checkBox1.TabIndex = 12;
+            this.checkBox1.Text = "Vetor";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            this.checkBox1.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(737, 374);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.SetCompress);
+            this.Controls.Add(this.SetLen);
+            this.Controls.Add(this.Setid);
+            this.Controls.Add(this.comprimido);
+            this.Controls.Add(this.tamanho);
+            this.Controls.Add(this.ID);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.richTextBox3);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.richTextBox2);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.button1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Decrypt Tool - Created By Hiro";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.RichTextBox richTextBox2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.RichTextBox richTextBox3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label ID;
+        private System.Windows.Forms.Label tamanho;
+        private System.Windows.Forms.Label comprimido;
+        private System.Windows.Forms.Label Setid;
+        private System.Windows.Forms.Label SetLen;
+        private System.Windows.Forms.Label SetCompress;
+        private System.Windows.Forms.CheckBox checkBox1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,164 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Globalization;
+
+namespace DES
+{
+    public partial class Form1 : Form
+    {
+        bool isVetor = false;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public static byte[] StringFromHex(string hex)
+        {
+            hex = hex.Replace(" ", "");
+            byte[] raw = new byte[hex.Length / 2];
+            for (int i = 0; i < raw.Length; i++)
+            {
+                raw[i] = Convert.ToByte(hex.Substring(i * 2, 2), 16);
+            }
+            return raw;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //byte[] iv = new byte[] { 0xCC, 0x6F, 0x4F, 0xA9, 0x7E, 0xD7, 0xB0, 0x62 };
+            byte[] iv = StringFromHex(textBox1.Text.Replace(" ",""));
+            byte[] key = StringFromHex(textBox1.Text.Replace(" ",""));
+            byte[] Setdata = StringFromHex(richTextBox1.Text.Replace(" ", ""));
+
+            try
+            {
+                if (Setdata.Length > 0)
+                {
+                    try
+                    {
+                        if (iv.Length > 0)
+                        {
+                            byte[] data = new byte[0];
+                            byte[] Decpacket = new byte[0];
+                            byte[] Header = new byte[16];
+                            int size = 0;
+
+                            Array.Resize(ref Decpacket, Setdata.Length - 10);//Chave de Autentitaçao
+                            Array.Copy(Setdata, 0, Decpacket, 0, Setdata.Length - 10);
+
+                            size = Decpacket.Length;
+                            byte[] SetDecpacket = new byte[0];
+
+                            Array.Resize(ref SetDecpacket, Decpacket.Length + 8);//Conteudo
+                            Array.Copy(Decpacket, 0, SetDecpacket, 8, size);
+
+                            byte[] descrypt = DESEncryption.DecryptData(SetDecpacket, iv, key);
+
+                            string packetDescrypt = BitConverter.ToString(descrypt).Replace("-", " ");
+                            byte[] finalize = StringFromHex(packetDescrypt.Substring(72));
+                            string packetDescrypt2 = BitConverter.ToString(finalize);
+
+                            richTextBox2.Text = BitConverter.ToString(finalize).Replace("-", " ");
+                            richTextBox3.Text = Encoding.ASCII.GetString(finalize);
+
+                            if (isVetor == true)
+                            {
+                                byte[] readVetor = new byte[StringFromHex(packetDescrypt.Substring(72)).Length];
+                                byte[] newVetor = new byte[8];
+                                readVetor = StringFromHex(packetDescrypt.Substring(72));
+                                Array.Copy(readVetor, 25, newVetor, 0, 8);
+                                textBox1.Text = BitConverter.ToString(newVetor).Replace("-", " ");
+                            }
+
+                            getId(finalize);
+                            getCompress(finalize[6]);
+                            getLen(finalize);
+                        }
+                    }
+                    catch (Exception ex)
+                    {
+                        MessageBox.Show(ex.Message);
+                    }
+                }
+                else
+                {
+                    Clear();
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message,ex.Source);
+            }
+            //richTextBox2.Text = BitConverter.ToString(DESEncryption.EncryptData(data,iv,key)).Replace("-"," ");
+        }
+
+        private void getLen(byte[] finalize)
+        {
+            SetLen.Text = Convert.ToString(finalize.Length);
+        }
+        /*private void getLen(byte[] finalize)
+        {            
+            string id = BitConverter.ToString(finalize, 0).Replace("-", "");
+            byte[] setlen = new byte[2];
+            setlen[0] = finalize[0];
+            setlen[1] = finalize[1];
+            Array.Reverse(setlen);
+            short num = BitConverter.ToInt16(setlen, 0);
+            SetLen.Text = Convert.ToString(num);
+        }*/
+
+        private void getCompress(byte Compressor)
+        {            
+            if (Compressor == 0)
+            {
+                SetCompress.Text = "Não";
+            }
+            else if (Compressor == 1)
+            {
+                SetCompress.Text = "Sim";
+            }
+        }
+
+        private void getId(byte[] finalize)
+        {
+            string id = BitConverter.ToString(finalize, 0).Replace("-", "");
+            byte[] setid = new byte[2];
+            setid[0] = finalize[0];
+            setid[1] = finalize[1];
+            Array.Reverse(setid);
+            short num = BitConverter.ToInt16(setid, 0);
+            Setid.Text = Convert.ToString(num);
+        }
+        private void Clear()
+        {
+            Setid.Text = "";
+            SetLen.Text = "";
+            SetCompress.Text = "";
+            richTextBox2.Text = "";
+            richTextBox3.Text = "";
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+            if (checkBox1.Checked)
+            {
+                isVetor = true;
+                textBox1.Text = "C7 D8 C4 BF B5 E9 C0 FD";
+            }
+            else
+            {
+                isVetor = false;
+            }
+        }        
+    }
+}
